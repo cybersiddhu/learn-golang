47427414b08ec46a980b281b51729b22417d72c0
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2013-06-28 16:56:40 -0500

    commented out test code

--- seqio/main.go
+++ seqio/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	 "github.com/cybersiddhu/gobio/seqio"
-	 "os"
-	 "fmt"
+	//	"fmt"
+	"github.com/cybersiddhu/gobio/seqio"
+	"os"
 )
-
 
 func main() {
 	r := seqio.NewFastaReader(os.Args[1])
-	for {
-		f, err := r.NextSeq()
-		if err != nil {
-			break
-		}
-		fmt.Fprintf(os.Stdout, "id:%s\tsequence:%s\n", f.Id, f.Sequence)
+	for r.HasEntry() {
+		//f := r.NextEntry()
+		//fmt.Fprintf(os.Stdout, "Entry>>>>\n Id:%s\tsequence:%s\n", f.Id, f.Sequence)
 	}
 }
