1e01f8b787b9cd95d0f2ca1452bbd00a4260a777
Siddhartha Basu <biosidd@gmail.com>
2013-06-29 07:25:08 -0500

    added back the previous code

--- seqio/main.go
+++ seqio/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//	"fmt"
+	"fmt"
 	"github.com/cybersiddhu/gobio/seqio"
 	"os"
 )
@@ -9,7 +9,7 @@ import (
 func main() {
 	r := seqio.NewFastaReader(os.Args[1])
 	for r.HasEntry() {
-		//f := r.NextEntry()
-		//fmt.Fprintf(os.Stdout, "Entry>>>>\n Id:%s\tsequence:%s\n", f.Id, f.Sequence)
+		f := r.NextEntry()
+		fmt.Fprintf(os.Stdout, "Entry>>>>\n Id:%s\tsequence:%s\n", f.Id, f.Sequence)
 	}
 }
